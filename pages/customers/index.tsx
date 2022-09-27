67fef0199c60fc0fc84a818609c11fcd2519e6f8
Caleb <caleb@calebcurry.com>
2022-09-27 11:38:31 -0400

    practice with getStaticProps

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,7 +1,42 @@
-import { NextPage } from 'next';
+import { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next';
+import axios from 'axios';
 
-const Customers: NextPage = () => {
-    return <h1>Customers</h1>;
+type Customer = {
+    id: number;
+    name: string;
+    industry: string;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const result = await axios.get<{
+        customers: Customer[];
+    }>('http://127.0.0.1:8000/api/customers/');
+    console.log(result.data.customers);
+
+    return {
+        props: {
+            customers: result.data.customers,
+        },
+    };
+};
+
+const Customers: NextPage = ({
+    customers,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+    return (
+        <>
+            <h1>Customers</h1>
+            {customers.map((customer: Customer) => {
+                return (
+                    <div key={customer.id}>
+                        <p>{customer.name}</p>
+                        <p>{customer.industry}</p>
+                        <p>{customer.id}</p>
+                    </div>
+                );
+            })}
+        </>
+    );
 };
 
 export default Customers;
--- pages/orders/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from 'next';
-
-const Orders: NextPage = () => {
-    return <h1>All orders</h1>;
-};
-
-export default Orders;
