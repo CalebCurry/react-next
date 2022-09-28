fdeca4f75471c6eb1190fb311559e0eddaffe0c7
Caleb <caleb@calebcurry.com>
2022-09-28 06:52:16 -0400

    add revalidate

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -17,6 +17,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
         props: {
             customers: result.data.customers,
         },
+        revalidate: 60,
     };
 };
 
