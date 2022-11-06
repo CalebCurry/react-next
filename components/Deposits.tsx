fe968a40efce2eeded52fcf3a2e60c7ce20215e9
Caleb <caleb@calebcurry.com>
2022-11-06 12:03:41 -0500

    change deposit value

--- components/Deposits.tsx
+++ components/Deposits.tsx
@@ -12,7 +12,7 @@ export default function Deposits() {
         <React.Fragment>
             <Title>Recent Deposits</Title>
             <Typography component="p" variant="h4">
-                $3,024.00
+                $0.00
             </Typography>
             <Typography color="text.secondary" sx={{ flex: 1 }}>
                 on 15 March, 2019
